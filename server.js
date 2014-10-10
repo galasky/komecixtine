b40c4995b7850a25252e25fddeab6ebb5bd8d2ce
lambert <ttrebmal@gmail.com>
2014-10-10 07:11:29 -0700

    jade

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
-  res.send('Hello World!');
-});
+app.set('views', './views');
+app.set('view engine', 'jade');
+app.engine('jade', require('jade').__express);
+
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Hey', message: 'Hello there!'});
+})
 
 var server = app.listen(3000, function() {
     console.log('Listening on port %d', server.address().port);
