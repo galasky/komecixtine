1a71f633fab841f1cc29bbf5e21a9ba63545d9f2
lambert <ttrebmal@gmail.com>
2014-10-10 03:18:38 -0700

    test

--- lp.py
+++ lp.py
@@ -1,12 +1,14 @@
 # wsgi_longpolling/simple.py
+import gevent
 from gevent import pywsgi
-
 
 def handle(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
-    yield '<html><body><h1>Hi There</h1></body></html>'
+    yield ' ' * 1200
+    yield '<html><body><h1>Hi '
+    gevent.sleep(10)
+    yield 'There</h1></body></html>'
 
-
-server = pywsgi.WSGIServer(('127.0.0.1', 1234), handle)
+server = pywsgi.WSGIServer(('0.0.0.0', 1234), handle)
 print "Serving on http://127.0.0.1:1234..."
 server.serve_forever()
