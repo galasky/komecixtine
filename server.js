04c8b11f1fdb25a68b3bcd1b6fb76df96b68f874
lambert <ttrebmal@gmail.com>
2014-10-10 08:50:37 -0700

    ok

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 app.set('views', './views');
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
+app.use("/static", express.static(path.join(__dirname, 'static')));
 
-app.get('/', function (req, res) {
-    res.render('index',{title:"my home page"});
+app.get('/', function (req, res, next) {
+    res.render('index',{title:"my home page"}, function(err, html) {
+	if (err) return next(err);
+	res.end(html);
+    });
 })
 
 var server = app.listen(3000, function() {
