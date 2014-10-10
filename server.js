d6fff1d0f786aad794de3f828817397a37e7917b
lambert <ttrebmal@gmail.com>
2014-10-10 08:32:24 -0700

    est

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ var express = require('express');
 var app = express();
 
 app.set('views', './views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 app.get('/', function (req, res) {
-    res.render('index.html');
+    res.render('index',{title:"my home page"});
 })
 
 var server = app.listen(3000, function() {
