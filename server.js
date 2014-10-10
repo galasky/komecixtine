e41b4f9075ed084609677521dafedab58acb9489
lambert <ttrebmal@gmail.com>
2014-10-10 08:20:30 -0700

    est

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ var express = require('express');
 var app = express();
 
 app.set('views', './views');
-app.set('view engine', 'html');
+app.set('view engine', 'jade');
 app.engine('jade', require('jade').__express);
 
 app.get('/', function (req, res) {
-    res.render('index', { title: 'Hey', message: 'Hello there!'});
+    res.render('index.html');
 })
 
 var server = app.listen(3000, function() {
