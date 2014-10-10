bf1695410e839f2fb0dc3ac6fdb541a089dd3118
lambert <ttrebmal@gmail.com>
2014-10-10 08:23:10 -0700

    est

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ var express = require('express');
 var app = express();
 
 app.set('views', './views');
-app.set('view engine', 'jade');
-app.engine('jade', require('jade').__express);
 
 app.get('/', function (req, res) {
     res.render('index.html');
