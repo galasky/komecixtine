7051d201258d8173d4498cb792168dcfc99ffcb3
lambert <ttrebmal@gmail.com>
2014-10-12 05:15:35 -0700

    static

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 
+app.use("/static", express.static(__dirname + '/static'));
+
 app.get('/', function(req, res){
     fs.readFile(__dirname + '/views/index.html',function (err, data){
         res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
