00c83ca74c7c51488484609c9b6e146f0f217347
lambert <ttrebmal@gmail.com>
2014-10-12 04:49:04 -0700

    js

--- server.js
+++ server.js
@@ -1,20 +1,15 @@
 var express = require('express');
 var app = express();
-var path = require('path');
+var fs = require('fs');
 
-app.set('views', './views');
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-app.use("/static", express.static(path.join(__dirname, 'static')));
-
-app.get('/', function (req, res, next) {
-    res.render('index',{title:"my home page"}, function(err, html) {
-	if (err) return next(err);
-	res.end(html);
+app.get('/', function(req, res){
+    fs.readFile('./views/index.html',function (err, data){
+        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+        res.write(data);
+        res.end();
     });
-})
-
-var server = app.listen(3000, function() {
-    console.log('Listening on port %d', server.address().port);
 });
 
+var server = app.listen(8888, function() {
+    console.log('Listening on port %d', server.address().port);
+});
