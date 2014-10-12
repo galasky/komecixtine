630691f2a5e75ed82a6703c4231b401038499391
lambert <ttrebmal@gmail.com>
2014-10-12 07:05:08 -0700

    f

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.get('/', function (req, res) {
 });
 
 io.on('connection', function (socket) {
+    console.log("connection");
   socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
     console.log(data);
