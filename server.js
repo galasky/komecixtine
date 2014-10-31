8f77e899f839dbde66da36298c535386450e4ee6
lambert <ttrebmal@gmail.com>
2014-10-31 07:51:28 +0100

    maj

--- server.js
+++ server.js
@@ -55,24 +55,44 @@ app.get('/profil', function (req, res) {
 io.on('connection', function (socket) {
     console.log("connection");
 
-    socket.on("testRoomName", function(data){
+    socket.on("testCRoomName", function(data){
         if (data.room == "") {
             console.log("VIDE");
-            socket.emit("rspTestRoomName", {rsp: false});
+            socket.emit("rspTestCRoomName", {rsp: false});
         } else {
             var rsp = !(data.room in listRoom);
             console.log("rsp " + rsp);
-            socket.emit("rspTestRoomName", {rsp: rsp});
+            socket.emit("rspTestCRoomName", {rsp: rsp});
         }
     });
 
-    socket.on("testPseudo", function(data){
+    socket.on("testCPseudo", function(data){
         if (data.pseudo == "") {
-            socket.emit("rspTestPseudo", {rsp: false});
+            socket.emit("rspTestCPseudo", {rsp: false});
         } else {
-            socket.emit("rspTestPseudo", {rsp: true});
+            socket.emit("rspTestCPseudo", {rsp: true});
         }
     });
+
+    socket.on("testJRoomName", function(data){
+        if (data.room == "") {
+            console.log("VIDE");
+            socket.emit("rspTestJRoomName", {rsp: false});
+        } else {
+            var rsp = (data.room in listRoom);
+            console.log("rsp " + rsp);
+            socket.emit("rspTestJRoomName", {rsp: rsp});
+        }
+    });
+
+    socket.on("testJPseudo", function(data){
+        if (data.pseudo == "") {
+            socket.emit("rspTestJPseudo", {rsp: false});
+        } else {
+            socket.emit("rspTestJPseudo", {rsp: true});
+        }
+    });
+
 
     socket.on("hello", function(data) {
         console.log("hello!");
