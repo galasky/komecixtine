76b05439b9c45eea5798c9816eada41a400863f5
lambert <ttrebmal@gmail.com>
2014-11-04 16:04:03 +0100

    Stack Card

--- Player.js
+++ Player.js
@@ -20,7 +20,7 @@ Player.prototype.emitRefreshListPlayer = function() {
     this.socket.emit("refreshListPlayer", {listPlayer: list});
 };
 Player.prototype.emitStart = function() {
-    this.socket.emit("start", {hands: this.hands});
+    this.socket.emit("start");
 };
 Player.prototype.takeCards = function(number) {
     this.hands = this.room.stack.pop(number);
