b2505856894385ce4d01f51061d8f2882e888aa9
lambert <ttrebmal@gmail.com>
2014-11-10 10:58:28 +0100

    enchant

--- static/js/main.js
+++ static/js/main.js
@@ -39,7 +39,7 @@ window.onload = function() {
             this.addChild(bg);
             this.addChild(new Carte(game.width/2, 280 * .25, 2, 0, false));
             this.addChild(new Carte(game.width/2 + 192 *.25, 280, 5, 2, false));
-            this.addChild(new Carte(game.width/2, 280 * .25 + 279 * .25, 8, 1, true));
+            this.addChild(new Carte(game.width/2, 280 + 279 * .25, 8, 1, true));
             this.addChild(new Carte(game.width/2 + 192 * .25, 280 + 279 *.25, 1, 3, true));
             this.addChild(label);
 //            this.addEventListener(Event.TOUCH_START,this.handleTouchControl);
