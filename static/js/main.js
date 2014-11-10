03d3dfbb48b920baab6f43cbdb64ec07de07848b
lambert <ttrebmal@gmail.com>
2014-11-10 13:51:38 +0100

    Stack

--- static/js/main.js
+++ static/js/main.js
@@ -117,11 +117,6 @@ window.onload = function() {
                 this.yInit = this.scene.stack.y;
                 this.show();
             }
-            if (this.x > 185 && this.x < 228 + 42 && this.y > 145 && this.y < 210) {
-                this.xInit = game.width * .5 - 192 * .5 - 192 / 2 * .25 + 192 * .25;
-                this.yInit = game.height * .25;
-                this.show();
-            }
             console.log("x = " + this.x + " y = " + this.y);
             this.x = this.xInit;
             this.y = this.yInit;
