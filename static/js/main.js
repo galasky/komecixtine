1b8a5b43df39ddaeb1982740294bf424063569b5
lambert <ttrebmal@gmail.com>
2014-11-12 12:14:37 +0100

    Stack

--- static/js/main.js
+++ static/js/main.js
@@ -69,14 +69,14 @@ window.onload = function() {
             }
         },
         malus: function() {
-            this.malu++;
+
             var m = new Carte(true, this, game.width * .5 - 192 * .5 - 192 / 2 * .25, game.height * .25, 1, 3, true);
             m.droped = true;
             this.hand.push(m);
             this.addChild(m);
-            m.xInit = game.width * .5 - 192 * .5 + 192 / 2 * .25 + 192 * .25 * this.malu;
-            m.yInit = 585 - 279;
-            console.log("malus");
+            m.xInit = game.width * .5 - 192 * .5 + 192 / 2 * .25 + 192 * .25 * ((this.malu / 2) + 1);
+            m.yInit = 585 - 279 + (this.malu % 2 == 1 ? 279 * .25 : 0);
+            this.malu++;
         },
         deletation: function(carte) {
             var tmp = [];
