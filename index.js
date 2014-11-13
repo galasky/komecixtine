88d89925b717dfd4cb67a20e7b26b6706c154d10
lambert <ttrebmal@gmail.com>
2014-11-13 17:03:59 +0100

    Stack

--- index.js
+++ index.js
@@ -65,3 +65,37 @@ app.set('view engine', 'handlebars');
 var port = process.env.PORT || 5000; //select your port or let it pull from your .env file
 app.listen(port);
 console.log("listening on " + port + "!");
+
+//===============ROUTES=================
+//displays our homepage
+app.get('/', function(req, res){
+    res.render('home', {user: req.user});
+});
+
+//displays our signup page
+app.get('/signin', function(req, res){
+    res.render('signin');
+});
+
+//sends the request through our local signup strategy, and if successful takes user to homepage, otherwise returns then to signin page
+app.post('/local-reg', passport.authenticate('local-signup', {
+        successRedirect: '/',
+        failureRedirect: '/signin'
+    })
+);
+
+//sends the request through our local login/signin strategy, and if successful takes user to homepage, otherwise returns then to signin page
+app.post('/login', passport.authenticate('local-signin', {
+        successRedirect: '/',
+        failureRedirect: '/signin'
+    })
+);
+
+//logs user out of site, deleting them from the session, and returns to homepage
+app.get('/logout', function(req, res){
+    var name = req.user.username;
+    console.log("LOGGIN OUT " + req.user.username)
+    req.logout();
+    res.redirect('/');
+    req.session.notice = "You have successfully been logged out " + name + "!";
+});
